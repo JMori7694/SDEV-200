4b2274bed7f578dc95ee203b08703b32cb997b7b
Jei <seal21295@gmail.com>
2023-08-23 15:40:13 -0400

    Initial commit

---
+++ 0357673425_663333/Farrell_JavaProg_10e_ch01_DataFiles/YouDoItCh1/Hello2.java
@@ -0,0 +1,12 @@
+// Filename Hello2.java
+// Written by Sarah Lesher
+// Written on 8/23/23
+
+public class Hello2 {
+    /* This class demonstrates the use of the println() 
+     * method to print the message Hello, World!
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
