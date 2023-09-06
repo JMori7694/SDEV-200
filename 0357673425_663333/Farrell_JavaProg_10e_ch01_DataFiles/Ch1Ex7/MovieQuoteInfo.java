0fc3d389cb34650879e4bd407867021d5c672f3a
Jei <seal21295@gmail.com>
2023-09-06 13:55:15 -0400

    Ch1Ex7

---
+++ 0357673425_663333/Farrell_JavaProg_10e_ch01_DataFiles/Ch1Ex7/MovieQuoteInfo.java
@@ -0,0 +1,7 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("\"Imagine dying frightened and in pain and having that as the only part of you which survives.\"");
+        System.out.println("Annihilation (2018)");
+        System.out.println("Josie Radek");
+    }
+}
