7c2a8f52096d6ddafac2618a3afb6e61af95ce55
Jei <seal21295@gmail.com>
2023-10-11 14:26:39 -0400

    update files

--- 0357673425_663333/Farrell_JavaProg_10e_ch04_DataFiles/YouDoItCh4/CreateSpaServices.java
+++ 0357673425_663333/Farrell_JavaProg_10e_ch04_DataFiles/YouDoItCh4/CreateSpaServices.java
@@ -2,38 +2,34 @@ import java.util.Scanner;
 
 public class CreateSpaServices {
     public static void main(String[] args) {
-        String service;
-        double price;
 
         SpaService firstService = new SpaService();
         SpaService secondService = new SpaService();
 
+        firstService = getData(firstService);
+        secondService = getData(secondService);
+
+        System.out.println("First service details:");
+        System.out.println(firstService.getServiceDescription() + " $" + firstService.getPrice());
+        System.out.println("Second service details:");
+        System.out.println(secondService.getServiceDescription() + " $" + secondService.getPrice());
+    }
+
+    public static SpaService getData(SpaService service) {
+        String serviceDescription;
+        double price;
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Enter service >> ");
-        service = keyboard.nextLine();
+        serviceDescription = keyboard.nextLine();
         System.out.print("Enter price >> ");
         price = keyboard.nextDouble();
         keyboard.nextLine();
 
-        firstService.setServiceDescription(service);
-        firstService.setPrice(price);
-
-        keyboard.nextLine();
-        System.out.print("Enter service >> ");
-        service = keyboard.nextLine();
-        System.out.print("Enter price >> ");
-        price = keyboard.nextDouble();
-
-        secondService.setServiceDescription(service);
-        secondService.setPrice(price);
-
-        System.out.println("First service details: ");
-        System.out.println(firstService.getServiceDescription() + " $" + firstService.getPrice());
-
-        System.out.println("Second service details: ");
-        System.out.println(secondService.getServiceDescription() + " $" + secondService.getPrice());
-
+        service.setServiceDescription(serviceDescription);
+        service.setPrice(price);
+        
         keyboard.close();
+        return service;
     }
 }
